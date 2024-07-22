d71fdc65e40a88a0997b7c7d7b60aef759a2d003
Thiago Brito <thiago7brito@gmail.com>
2024-07-22 13:48:38 -0300

    counte created

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,25 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <p>{{ count }}</p>
+    <button @click="sub">Sub</button>
+    <button @click="add">Add</button>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<script>
+  export default {
+    data() {
+      return {
+        count: 0,
+      }
+    },
+    methods: {
+      add() {
+        this.count++;
+      },
+      sub() {
+        this.count < 1 || this.count--;
+      },
+    },
+  };
+</script>
